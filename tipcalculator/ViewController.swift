5bb59af05e7ef9e089336152ae62cc0ab8a47ea5
Mac <mac@mac-106.local>
2016-11-15 18:58:50 +0800

    小費計算APP

--- tipcalculator/ViewController.swift
+++ tipcalculator/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var money: UITextField!  //輸入金額
+    @IBOutlet weak var tip: UITextField!  //輸入小費
+    @IBOutlet weak var result: UILabel!  //小費計算結果
+    
+    //計算按鈕
+    @IBAction func calculate(_ sender: UIButton) {
+        
+         money.resignFirstResponder()
+         tip.resignFirstResponder()
+
+         result.isHidden = false
+         result.text = String(Double(money.text!)! * Double(tip.text!)! / 100)
+    }
+    
+    //按按鈕收鍵盤
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        money.resignFirstResponder()
+        tip.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    //按任意地方收鍵盤
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         self.hideKeyboardWhenTappedAround()
     }
 
     override func didReceiveMemoryWarning() {
